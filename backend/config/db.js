d428d1324fa97b5298451bd9b9127512a2a8d49b
Aahwan verma <vaahwan@gmail.com>
2024-05-10 06:38:07 +0530

    backend almost done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,2 +1,8 @@
 //mongodb+srv://vaahwan:vaahwan@cluster0.xxaleu8.mongodb.net/rurux
 
+const mongoose = require("mongoose")
+const url = "mongodb+srv://vaahwan:vaahwan@cluster0.xxaleu8.mongodb.net/rurux";
+
+const connection = mongoose.connect(url);
+
+module.exports = {connection};
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const {connection} = require("./config/db")
+const {studentRoute} = require("./routes/studentRoute")
+const {adminRoute} = require("./routes/adminRoute")
 
 const app = express();
+const port = 8000;
 
 app.use(express.json());
+
+app.use('/student',studentRoute);
+app.use('/admin',adminRoute);
 
 app.get('/',(req,res)=>{
     try{
@@ -13,6 +20,13 @@ app.get('/',(req,res)=>{
     }
 })
 
-app.listen(8000,()=>{
-    console.log("server is listioning at port 8000")
+app.listen(port,async()=>{
+    try{
+        await connection;
+        console.log(`server is listioning at port ${port}`);
+    }
+    catch(error){
+        console.log(error);
+    }
+    
 })
